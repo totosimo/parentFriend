6f21d302c81ab5e5c4359588ba551a80ea263bbc
Ad Astra <sergey.v.onoyko@gmail.com>
2021-03-04 11:59:55 +0100

    removed api token from public view through ENV

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -14,32 +14,38 @@ console.warn(`ERROR(${err.code}): ${err.message}`);
 };
 
 //Obtain current position of the client
-const initCurrentPosition = () => {navigator.geolocation.getCurrentPosition((position, error, options) => {
+const initCurrentPosition = () => {
+
+    const mapElement = document.getElementById('mapusers');
+
+    navigator.geolocation.getCurrentPosition((position, error, options) => {
     const pa = position.coords.latitude;
     const pb = position.coords.longitude;
     const str_gsd = `latitude=${pa}&longitude=${pb}`
 
-//Call DB to update current_user position values
-    Rails.ajax({
-        url: "/main",
-        dataType: 'json',
-        type: "post",
-        data: str_gsd,
-    })
+    //Call DB to update current_user position values
+        Rails.ajax({
+            url: "/main",
+            dataType: 'json',
+            type: "post",
+            data: str_gsd,
+        })
 
-//Display the map
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZ2VvcmdpdXMtc3RvaWN1cyIsImEiOiJja2tzZDN1dW8wbW16MndwY2k5ejBncnl6In0.l9u8Fk9ypU9iOM_wUXTAlA';
-    const map = new mapboxgl.Map({
-        container: 'mapusers',
-        style: 'mapbox://styles/mapbox/streets-v9',
-        center: [pb, pa],
-        zoom: 14
-    });
+    //Display the map
+        if (mapElement) { // only build a map if there's a div#map to inject into
+            mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+                const map = new mapboxgl.Map({
+                container: 'mapusers',
+                style: 'mapbox://styles/mapbox/streets-v9',
+                center: [pb, pa],
+                zoom: 14
+            });
 
-//Put a marker on the map
-    const marker = new mapboxgl.Marker()
-    .setLngLat([pb, pa])
-    .addTo(map);
+            //Put a marker on the map
+            const marker = new mapboxgl.Marker()
+            .setLngLat([pb, pa])
+            .addTo(map);
+        };
     });
 };
 
