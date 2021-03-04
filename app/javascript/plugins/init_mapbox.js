0bb00d722252d501d9a4b929b7acd2d7e0775d22
Ad Astra <sergey.v.onoyko@gmail.com>
2021-03-04 16:27:36 +0100

    coded event card, added it to event index view, added event popup windows on events map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -18,8 +18,10 @@ const initMapbox = () => {
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup) // add this
         .addTo(map);
     });
     fitMapToMarkers(map, markers);
