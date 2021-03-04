646f27dedc0a6ccf3af50c19c5dfd4573636049b
Simone Totino <totino.simone@gmail.com>
2021-03-04 23:37:29 +0200

    modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,6 @@ ActiveRecord::Schema.define(version: 2021_03_04_194340) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_chatrooms_on_user_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -75,7 +73,6 @@ ActiveRecord::Schema.define(version: 2021_03_04_194340) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "chatrooms", "users"
   add_foreign_key "events", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
