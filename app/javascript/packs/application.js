cc81b556e6e7d970fcdd772b3159d266240c1df6
Simone Totino <totino.simone@gmail.com>
2021-03-05 14:23:12 +0200

    added flatpickr and started beautifying the chat index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,8 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { initFlatpickr } from "../plugins/flatpickr";
+initFlatpickr();
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
