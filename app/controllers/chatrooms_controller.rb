6b019ed850398ecde8fa5047746189327dbf1dc0
Simone Totino <totino.simone@gmail.com>
2021-03-08 19:47:18 +0200

    added user routes and methods #index and #show. added chatroom #create

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -9,4 +9,19 @@ class ChatroomsController < ApplicationController
     @message = Message.new
     authorize @chatroom
   end
+
+  def create
+    @chatroom = Chatroom.create(name: 'Fantasy Chat Two')
+    authorize @chatroom
+    @user_chatroom = UserChatroom.create(user: current_user, chatroom: @chatroom)
+    @user_chatroom_two = UserChatroom.create(user: User.find(params[:user_id]), chatroom: @chatroom)
+    # @user_chatroom.chatroom = @chatroom
+    # @user_chatroom.user = current_user
+    # @user_chatroom.user = current_user
+    raise
+    # @user_chatroom = UserChatroom.new(user_id =current_user, chatroom_id = @chatroom)
+
+
+  end
+
 end
