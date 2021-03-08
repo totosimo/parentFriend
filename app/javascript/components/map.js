42a055a6ec4fec1a31597d407acde5cbe932ef02
Ad Astra <sergey.v.onoyko@gmail.com>
2021-03-08 19:32:53 +0100

    added styling to user icons

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -30,7 +30,7 @@ const initCurrentPosition = () => {
             const pb = position.coords.longitude;
             const str_gsd = `latitude=${pa}&longitude=${pb}`
 
-            //Call DB to update current_user position values
+            // Call DB to update current_user position values
             Rails.ajax({
                 url: "/meet",
                 dataType: 'json',
@@ -75,69 +75,3 @@ const initCurrentPosition = () => {
 
 //Export Function
 export { initCurrentPosition };
-
-
-//HOW TO DETERMINE CURRENT POSITION WITH PURE JAVASCRIPT
-// import mapboxgl from 'mapbox-gl';
-// import Rails from '@rails/ujs';
-
-// //Enable High Accuracy
-// const options = {
-//     enableHighAccuracy: true,
-//     timeout: 5000,
-//     maximumAge: 0
-//   };
-
-// //Define Error Handling
-// const error = (err) => {
-// console.warn(`ERROR(${err.code}): ${err.message}`);
-// };
-
-// //Obtain current position of the client
-// const initCurrentPosition = () => {
-
-//     const mapElement = document.getElementById('mapUsers');
-
-//     if (mapElement) {
-//         navigator.geolocation.getCurrentPosition((position, error, options) => {
-//         const pa = position.coords.latitude;
-//         const pb = position.coords.longitude;
-//         const str_gsd = `latitude=${pa}&longitude=${pb}`
-
-//         //Call DB to update current_user position values
-//             Rails.ajax({
-//                 url: "/meet",
-//                 dataType: 'json',
-//                 type: "post",
-//                 data: str_gsd,
-//             })
-
-//         //Display the map
-//             if (mapElement) { // only build a map if there's a div#map to inject into
-//                 mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-//                     const map = new mapboxgl.Map({
-//                     container: 'mapUsers',
-//                     style: 'mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb',
-//                     center: [pb, pa],
-//                     zoom: 14
-//                 });
-//                 //Add geolocation control to the map
-//                 map.addControl(
-//                     new mapboxgl.GeolocateControl({
-//                     positionOptions: {
-//                         enableHighAccuracy: true
-//                     },
-//                     trackUserLocation: true
-//                     })
-//                 );
-//                 //Put a marker on the map
-//                 const marker = new mapboxgl.Marker()
-//                 .setLngLat([pb, pa])
-//                 .addTo(map);
-//             };
-//         });
-//     };
-// };
-
-// //Export Function
-// export { initCurrentPosition };
