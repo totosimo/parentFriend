504bfba9a1fc2493ab49102c0005ecb504719075
Ad Astra <sergey.v.onoyko@gmail.com>
2021-03-09 17:20:27 +0100

    cleaned up routes, policies, actions and views for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,7 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: [:show, :destroy]
+  before_action :set_booking, only: [:destroy]
   def index
     @bookings = policy_scope(Booking).order(created_at: :desc)
-  end
-
-  def show
-    authorize @booking
   end
 
   def destroy
