8d8acff85045c37db21219d363087c757964dac1
Ad Astra <sergey.v.onoyko@gmail.com>
2021-03-09 11:47:10 +0100

    generated bookings controller and views

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,22 @@
+class BookingsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
